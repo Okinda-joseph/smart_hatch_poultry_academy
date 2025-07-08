9d6b016fd05d232291dc41bf536b400e509e5a8a
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2025-07-08 16:32:16 +0300

    Added files

---
+++ hero.php
@@ -0,0 +1,45 @@
+
+<div class="owl-carousel owl-theme">
+            <div class="item">
+                <h4>1</h4>
+            </div>
+            <div class="item">
+                <h4>2</h4>
+            </div>
+            <div class="item">
+                <h4>3</h4>
+            </div>
+            <div class="item">
+                <h4>4</h4>
+            </div>
+            <div class="item">
+                <h4>5</h4>
+            </div>
+            <div class="item">
+                <h4>6</h4>
+            </div>
+            <div class="item">
+                <h4>7</h4>
+            </div>
+            <div class="item">
+                <h4>8</h4>
+            </div>
+            <div class="item">
+                <h4>9</h4>
+            </div>
+            <div class="item">
+                <h4>10</h4>
+            </div>
+            <div class="item">
+                <h4>11</h4>
+            </div>
+            <div class="item">
+                <h4>12</h4>
+            </div>
+        </div>
+
+
+        <i class="bx bx-phone-call"></i>
+        <i class="bx bxs-bed"></i>
+        <i class="bx bx-envelope"></i>
+        <i class="bx bxl-github"></i>
